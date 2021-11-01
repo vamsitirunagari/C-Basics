47f505345037588a8cb9f8b46b42552127f8c0ba
vamsitirunagari <84132163+vamsitirunagari@users.noreply.github.com>
2021-11-01 10:18:08 +0530

    Add files via upload

---
+++ operator1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a=8, b=4;
+	sum=a+b;
+	diff=a-b;
+	mult=a*b;
+	div=a/b;
+	mod_div=a%b;
+	printf("Sum of a and b: %d\n", sum);
+	printf("Difference between a and b: %d\n", diff);
+	printf("Product of a and b: %d\n", mult);
+	printf("Quotient when a is divided by b: %d\n", div);
+	printf("Remainder when a is divided by b: %d\n", mod);
+}
