4e09a03a56dbc66fd9a1a7ad29a98f0497d3c09f
vamsitirunagari <84132163+vamsitirunagari@users.noreply.github.com>
2021-11-03 10:01:51 +0530

    Add files via upload

---
+++ minutes into hours and minutes.c
@@ -0,0 +1,15 @@
+//write a c program that converts minutes into hours and minutes
+#include<stdio.h>
+void main()
+{
+	int minutes,x,y;
+	minutes=550;
+	x=minutes/60;
+	y=minutes%60;
+	
+	
+	
+	printf("%d hour(s) %d minute(s)",x,y);
+	
+}
+
