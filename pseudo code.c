1000bab7a943628931cfa18439f7facf5a856fb5
vamsitirunagari <84132163+vamsitirunagari@users.noreply.github.com>
2021-12-13 10:51:21 +0530

    Add files via upload

---
+++ pseudo code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int p,q,r;
+	printf("Enter the numbers");
+	scanf("%d%d%d",&p,&q,&r);
+	if((r>p) || (7-7)>(r-7))
+	{
+		q=p;
+	}
+	else
+	{
+		p=12;
+	}
+	printf("%d",p+q+r);
+}
