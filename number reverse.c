83b06e10cdee04921e217a65440795e76661a97e
vamsitirunagari <84132163+vamsitirunagari@users.noreply.github.com>
2022-03-03 11:05:07 +0530

    Add files via upload

---
+++ number reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,reverse=0;
+	printf("enter a number: ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		reverse=reverse*10+r;
+		n=n/10;
+		
+		
+	}
+	printf("%d",reverse);
+}
